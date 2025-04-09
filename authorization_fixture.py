cf143e409dfb427d0f785b31c9a7962308cdcebc
vladtornikov <balesiy@gmail.com>
2025-04-10 02:21:53 +0700

    Add files via upload

--- authorization_fixture.py
+++ authorization_fixture.py
@@ -1,14 +1,11 @@
-@pytest.fixture(scope='session', autouse=True)
-async def authenticated_ac(ac: httpx.AsyncClient, register_user):
-    response = await ac.post(
-        '/auth/login',
-        json = {
-            'email': 'kot@pes.com',
-            'password': 'jack123@rambler.ru'
-        }
-    )
-    assert response.status_code == 200
-    token = response.cookies.get('access_token')
-    assert token
-    ac.headers.update({'Authorization': f'Bearer {token}'})
-    return ac
+@pytest.fixture(scope="session")
+ async def authenticated_ac(register_user, ac):
+     await ac.post(
+         "/auth/login",
+         json={
+             "email": "kot@pes.com",
+             "password": "1234"
+         }
+     )
+     assert ac.cookies["access_token"]
+     yield ac
