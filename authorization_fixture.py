d6144c0502fbbd4edbb5c2d6a76d375b7b70fc9b
vladtornikov <balesiy@gmail.com>
2025-04-10 01:46:59 +0700

    fixture in API test to authorize user

---
+++ authorization_fixture.py
@@ -0,0 +1,13 @@
+@pytest.fixture(scope='session', autouse=True)
+async def authenticated_ac(ac: httpx.AsyncClient, register_user):
+    response = await ac.post(
+        '/auth/login',
+        json = {
+            'email': 'kot@pes.com',
+            'password': 'jack123@rambler.ru'
+        }
+    )
+    assert response.status_code == 200
+    token = response.cookies.get('access_token')
+    assert token
+    return {'Authorization': f'Bearer {token}'}
